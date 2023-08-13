70a4fdb38402a7a1b8d00c68ea6d9364d1c02ddb
chaithanyabtech <140719067+chaithanyabtech@users.noreply.github.com>
2023-08-14 02:40:52 +0530

    Add files via upload

--- Assignment-1/codes/main.py
+++ Assignment-1/codes/main.py
@@ -9,9 +9,9 @@ O = np.array ([ -4.4167 , 0.4167 ])
 O_1 = O - A
 O_2 = O - B
 O_3 = O - C
-a = np.linalg.norm(np.transpose(O_1) @ (O_1))
-b = np.linalg.norm(np.transpose(O_2) @ (O_2))
-c = np.linalg.norm(np.transpose(O_3) @ (O_3))
+a = np.linalg.norm(O_1)
+b = np.linalg.norm(O_2)
+c = np.linalg.norm(O_3)
 print("Points of triangle A, B, C respectively are", A ,",", B ,",", C, ".")
 print("Circumcentre of triangle is", O, ".")
 print(" OA, OB, OC are respectively", a,",", b,",",c, ".")
